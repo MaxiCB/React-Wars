a70986a8c5bacf0577fd1f4fbccd58d5c2117269
MaciXB <aaroncb@aaroncb.com>
2020-01-24 11:27:41 -0600

    ADD Pagination
    - Implemented Pagination into App
    - Created dynamic function to create pagination items rather than repeating code

--- react-wars/src/App.js
+++ react-wars/src/App.js
@@ -26,17 +26,63 @@ const  App = () => {
     fetchData();
   }, [pageQuery])
 
+  const onChangePage = (pageNum) => {
+    if(pageNum > 0 && pageNum < 10) {
+      console.log(pageNum)
+      setCurrentPage(pageNum);
+      setPageQuery(pageNum);
+      console.log(currentPage);
+      console.log(pageNum);
+    }
+  }
+
+  const max = 10;
+
+  const createPagination = () => {
+    let paginationitems = []
+    for (let index = 0; index < max; index++) {
+      let links = []
+      for (let i = 0; i < 1; i++) {
+      links.push(<PaginationLink onClick={e => onChangePage(index)}>{index}</PaginationLink>)     
+      }
+    paginationitems.push(<PaginationItem>{links}</PaginationItem>)
+    }
+    return paginationitems;
+  }
+
   return (
     <div className="App">
       <Container fluid={true}>
         {isLoading ? <Spinner color="primary" className="spinnerClass"/> : <></>}
         <Row  xs="4" className="personRow">
-        {apiData.map(item => (
-          <Col className="personCard">
-          <PersonCard {...item}/>
+        {apiData.map((item, index) => (
+          <Col className="personCard" key={index}>
+            <PersonCard {...item}/>
           </Col>
         ))}
         </Row>
+          <Row className="paginationRow">
+            <Pagination>
+              <PaginationItem>
+                {/* First */}
+                <PaginationLink first onClick={e => onChangePage(1)}/>
+              </PaginationItem>
+              <PaginationItem>
+                {/* Back by one */}
+                <PaginationLink previous onClick={e => onChangePage(pageQuery - 1)}/>
+              </PaginationItem>
+              {/* Dynamic creation of pagination items */}
+              {createPagination()}
+              <PaginationItem>
+                {/* Forward by one */}
+                <PaginationLink next onClick={e => onChangePage(pageQuery + 1)} />
+              </PaginationItem>
+              <PaginationItem>
+                {/* End */}
+                <PaginationLink last onClick={e => onChangePage(9)} />
+              </PaginationItem>
+            </Pagination>
+          </Row>
         </Container>
     </div>
   );
